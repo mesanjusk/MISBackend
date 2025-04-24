daf6d61492c41bb2e29ce0af5958558990aa449d
mesanjusk <116157981+mesanjusk@users.noreply.github.com>
2025-04-24 23:55:27 +0530

    Update

--- Models/taskgroup.js
+++ Models/taskgroup.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const TaskgroupSchema=new mongoose.Schema({
     Task_group_uuid: { type: String },
     Task_group: { type: String, required: true },
+    Id: { type: Number, required: true }
  })
 
  const  Taskgroup = mongoose.model(" Taskgroup",  TaskgroupSchema);
