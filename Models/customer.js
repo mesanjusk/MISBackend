a4f0e0b212b6eb2c635b923760c059c41017f0c6
sbframee <104187401+sbframee@users.noreply.github.com>
2025-05-02 22:39:52 +0530

    update

--- Models/customer.js
+++ Models/customer.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const CustomersSchema = new mongoose.Schema({
     Customer_uuid: { type: String },
     Customer_name: { type: String, required: true },
-    Mobile_number: { type: String, required: false, unique: false }, // made optional and non-unique
+    Mobile_number: { type: String }, 
     Customer_group: { type: String, required: true },
     Status: { type: String, default: 'active' },
     Tags: { type: [String], default: [] },
