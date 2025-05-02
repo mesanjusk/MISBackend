b3c711081dce956002158f25f657135a0e8eaa87
mesanjusk <116157981+mesanjusk@users.noreply.github.com>
2025-05-02 12:17:37 +0530

    Update custom

--- Models/customer.js
+++ Models/customer.js
@@ -5,9 +5,9 @@ const CustomersSchema = new mongoose.Schema({
     Customer_name: { type: String, required: true },
     Mobile_number: { type: Number, required: true, unique: true },
     Customer_group: { type: String, required: true },
-    Status: { type: String, default: 'active' },  // New field for customer status
-    Tags: { type: [String], default: [] },  // New field for customer tags (e.g., "unsatisfied", "high_value")
-    LastInteraction: { type: Date, default: Date.now },  // New field for last interaction date
+    Status: { type: String, default: 'active' }, // New field
+    Tags: { type: [String], default: [] }, // New field
+    LastInteraction: { type: Date, default: Date.now }, // New field
 });
 
 const Customers = mongoose.model("Customers", CustomersSchema);
