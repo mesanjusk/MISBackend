c02c12d55f53a72c15e9a95994852cb86f61fa8a
mesanjusk <mesanjusk@gmail.comm>
2025-05-04 14:48:53 +0530

    wps

--- Services/whatsappService.js
+++ Services/whatsappService.js
@@ -1,6 +1,6 @@
-// services/whatsappService.js
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
+const qrCodeToImage = require('qrcode'); // To generate a base64 QR image.
 
 function setupWhatsApp(io) {
   const client = new Client({
@@ -14,7 +14,15 @@ function setupWhatsApp(io) {
   client.on('qr', (qr) => {
     console.log('QR RECEIVED', qr);
     qrcode.generate(qr, { small: true });
-    io.emit('qr', qr);
+
+    // Convert the QR code to a base64 PNG image
+    qrCodeToImage.toDataURL(qr, (err, url) => {
+      if (err) {
+        console.error('Error generating QR image:', err);
+      } else {
+        io.emit('qr', url); // Emit the base64 image to the frontend
+      }
+    });
   });
 
   client.on('ready', () => {
