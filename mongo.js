e4ee2c40444a58b2b0e91e7dd83d1b4acb95a955
mesanjusk <116157981+mesanjusk@users.noreply.github.com>
2025-07-31 23:52:49 +0530

    12345

--- mongo.js
+++ mongo.js
@@ -2,8 +2,14 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://sanjuahuja:cY7NtMKm8M10MbUs@cluster0.wdfsd.mongodb.net/MISSK');
-    console.log('✅ MongoDB connected');
+     await mongoose.connect('mongodb+srv://sanjuahuja:cY7NtMKm8M10MbUs@cluster0.wdfsd.mongodb.net/MISSK', {
+      autoIndex: true,
+    });
+
+    // Ensure all indexes defined in schemas are created in MongoDB
+    await mongoose.connection.syncIndexes();
+
+    console.log('✅ MongoDB connected and indexes synced');
   } catch (error) {
     console.error('❌ MongoDB connection error:', error);
     process.exit(1);
