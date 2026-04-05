f82eb58a8129b93120d2ce58c9f8bbbd8eac4b43
mesanjusk <116157981+mesanjusk@users.noreply.github.com>
2026-04-05 13:44:03 +0530

    123456

---
+++ src/config/whatsappTemplates.js
@@ -0,0 +1,10 @@
+module.exports = {
+  DEFAULT_APPROVED_TEMPLATE: 'payment_received_sk',
+  TEMPLATE_USAGE: {
+    addTransaction: 'payment_received_sk',
+    addTransaction1: 'payment_received_sk',
+    addOrder1: 'payment_received_sk',
+    paymentFollowup: 'payment_received_sk',
+    addUsertask: 'payment_received_sk',
+  },
+};
