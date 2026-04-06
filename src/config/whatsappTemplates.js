44db793fbf81853cf76f44454cd6b4422a1e2935
mesanjusk <116157981+mesanjusk@users.noreply.github.com>
2026-04-06 10:21:09 +0530

    12345678

--- src/config/whatsappTemplates.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  PAYMENT_RECEIVED_TEMPLATE: 'payment_received_sk',
-  ORDER_CONFIRMATION_TEMPLATE: 'payment_received_sk',
-  FOLLOWUP_TEMPLATE: 'payment_received_sk',
-  DEFAULT_LANGUAGE: 'en_US',
-  ADMIN_ALERT_PHONE: '919372333633',
-};
